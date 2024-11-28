0c7e63a2bbcc94e19460a4e63189315336e36d36
sebybuga <seby_buga@yahoo.com>
2024-11-28 12:41:19 +0200

    Global ex handler added

---
+++ src/main/java/com/hubproductsmanagement/controller/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.hubproductsmanagement.controller;
+
+import com.hubproductsmanagement.exception.ProblemProcessingDataException;
+import org.flywaydb.core.api.ErrorCode;
+import org.flywaydb.core.api.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProblemProcessingDataException.class)
+    public ResponseEntity<> handleUserNotFoundException(ProblemProcessingDataException ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorDetails(ErrorCode.ERROR, ex.getMessage()), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<> handleUnauthorized(BadCredentialsException ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorDetails(ErrorCode.CONFIGURATION, ex.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<> handleAccessDenied(AccessDeniedException ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorDetails(ErrorCode.CONFIGURATION, ex.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<> handleGlobalException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorDetails(ErrorCode.ERROR, ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/com/hubproductsmanagement/exception/ProblemProcessingDataException.java
@@ -0,0 +1,7 @@
+package com.hubproductsmanagement.exception;
+
+public class ProblemProcessingDataException extends RuntimeException {
+    public ProblemProcessingDataException(String message) {
+        super(message);
+    }
+}
