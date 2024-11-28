197e43041bccf055f63b8ad6b5fba6de0a5d083e
sebybuga <seby_buga@yahoo.com>
2024-11-28 19:48:30 +0200

    Tests added

--- src/test/java/com/hubproductsmanagement/service/ProductServiceTest.java
+++ src/test/java/com/hubproductsmanagement/service/ProductServiceTest.java
@@ -2,10 +2,13 @@ package com.hubproductsmanagement.service;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
+import com.hubproductsmanagement.dto.DeleteResponseREC;
 import com.hubproductsmanagement.dto.ProductDTO;
 import com.hubproductsmanagement.dto.ProductRequestDTO;
 import com.hubproductsmanagement.entity.ProductEntity;
+import com.hubproductsmanagement.exception.ProblemProcessingDataException;
 import com.hubproductsmanagement.repo.ProductRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -16,9 +19,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-
 
 
 @RunWith(SpringRunner.class)
@@ -30,17 +35,21 @@ public class ProductServiceTest {
     @Mock
     ProductRepository productRepositoryMock;
 
-    private final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    private ProductRequestDTO productDTO;
+    private ProductEntity productEntity;
 
+    private final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    @Before
+    public void setUp() {
+        productDTO = generateProductDto();
+        productEntity = mapper.map(productDTO, ProductEntity.class);
+
+        when(productRepositoryMock.findById(any())).thenReturn(Optional.ofNullable(productEntity));
+    }
 
     @Test
     public void testCreateProduct() {
-
-        ProductRequestDTO productDTO = generateProductDto();
-        ProductEntity productEntity = mapper.map(productDTO, ProductEntity.class);
-
         when(productRepositoryMock.save(any())).thenReturn(productEntity);
-        when(productRepositoryMock.findById(any())).thenReturn(Optional.ofNullable(productEntity));
 
         ProductDTO savedProduct = productService.createProduct(productDTO);
 
@@ -49,16 +58,51 @@ public class ProductServiceTest {
 
     }
 
+    @Test
+    public void testUpdateProduct() {
+        when(productRepositoryMock.save(any())).thenReturn(productEntity);
+
+        ProductDTO savedProduct = productService.updateProduct(productDTO);
+
+        assertEquals(savedProduct.getProductName(),productDTO.getProductName());
+        assertEquals(savedProduct.getSupplier(),productDTO.getSupplier());
+
+    }
+    @Test
+    public void testGetProduct(){
+        ProductDTO savedProduct = productService.getProduct(productDTO.getId());
+
+        assertEquals(savedProduct.getProductName(),productDTO.getProductName());
+        assertEquals(savedProduct.getSupplier(),productDTO.getSupplier());
+
+    }
+
+    @Test
+    public void testDeleteProduct_success(){
+        when(productRepositoryMock.existsById(anyLong())).thenReturn(true);
+        DeleteResponseREC response = productService.deleteProduct(productDTO.getId());
+        String idToSearch = productDTO.getId().toString();
+        assertTrue(response.message().contains(idToSearch));
+
+    }
+
+    @Test
+    public void testDeleteProduct_fail(){
+        when(productRepositoryMock.existsById(anyLong())).thenReturn(false);
+        assertThrows(ProblemProcessingDataException.class, () ->
+                productService.deleteProduct(productDTO.getId()));
+
+    }
+
+
     ProductRequestDTO generateProductDto() {
+        ProductRequestDTO productRequestDTO = new ProductRequestDTO();
+        productRequestDTO.setId(1L);
+        productRequestDTO.setProductName("some name");
+        productRequestDTO.setDescription("some desc");
+        productRequestDTO.setSupplier("some supplier");
 
-        ProductRequestDTO productDTO = new ProductRequestDTO();
-        productDTO.setId(1L);
-        productDTO.setProductName("some name");
-        productDTO.setDescription("some desc");
-        productDTO.setSupplier("some supplier");
-
-        return productDTO;
-
+        return productRequestDTO;
     }
 
 
